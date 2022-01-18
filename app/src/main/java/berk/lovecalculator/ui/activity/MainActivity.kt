8e3d89f53f4ef24ade2b0bf4a2e26a8c5b9c6932
berk yalnızcık <byalnzck@gmail.com>
2022-01-18 08:20:24 +0300

    BackPressed disabled

--- app/src/main/java/berk/lovecalculator/ui/activity/MainActivity.kt
+++ app/src/main/java/berk/lovecalculator/ui/activity/MainActivity.kt
@@ -1,12 +1,10 @@
 package berk.lovecalculator.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import berk.lovecalculator.R
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onBackPressed() {
     }
 }

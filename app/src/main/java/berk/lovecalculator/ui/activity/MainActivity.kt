d031a370ae60cfdb4b64662fe207aeec87255b6e
berk yalnızcık <byalnzck@gmail.com>
2022-01-18 06:36:15 +0300

    Package structure done and Navigation created

--- app/src/main/java/berk/lovecalculator/ui/activity/MainActivity.kt
+++ app/src/main/java/berk/lovecalculator/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package berk.lovecalculator
+package berk.lovecalculator.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import berk.lovecalculator.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

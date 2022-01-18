7c7ae25f5d77b36c0527d16943bfe758e2993021
berk yalnızcık <byalnzck@gmail.com>
2022-01-18 08:18:49 +0300

    Navigation Actions created and safe args added

--- app/src/main/java/berk/lovecalculator/ui/fragment/HomeFragment.kt
+++ app/src/main/java/berk/lovecalculator/ui/fragment/HomeFragment.kt
@@ -1,5 +1,26 @@
 package berk.lovecalculator.ui.fragment
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import berk.lovecalculator.databinding.FragmentHomeBinding
 
-class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate)
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onClick()
+    }
+
+    private fun onClick() {
+        with(binding) {
+            btnGoToResult.setOnClickListener {
+                val yourName = edtTextYourName.text.toString()
+                val partnerName = edtTextPartnerName.text.toString()
+                val action =
+                    HomeFragmentDirections.actionHomeFragmentToResultFragment(yourName, partnerName)
+                findNavController().navigate(action)
+            }
+        }
+    }
+}
--- app/src/main/java/berk/lovecalculator/ui/fragment/ResultFragment.kt
+++ app/src/main/java/berk/lovecalculator/ui/fragment/ResultFragment.kt
@@ -1,5 +1,23 @@
 package berk.lovecalculator.ui.fragment
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import berk.lovecalculator.R
 import berk.lovecalculator.databinding.FragmentResultBinding
 
-class ResultFragment : BaseFragment<FragmentResultBinding>(FragmentResultBinding::inflate)
+class ResultFragment : BaseFragment<FragmentResultBinding>(FragmentResultBinding::inflate) {
+    private val args: ResultFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onClick()
+    }
+
+    private fun onClick() {
+        binding.btnGoToHome.setOnClickListener {
+            findNavController().navigate(R.id.action_resultFragment_to_homeFragment)
+        }
+    }
+}
